f8e7d730ce2fa6842bd34a3dfc6f9dc8b1ac323b
Sejaltalekar3 <96169999+Sejaltalekar3@users.noreply.github.com>
2023-07-19 20:09:06 +0530

    Assignments

---
+++ math/SquareRoot.java
@@ -0,0 +1,15 @@
+package com.kodnest.math;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the number n");
+		int n=scan.nextInt();
+		
+		int squareroot= (int)Math.sqrt(n);
+		System.out.println("the squareroot of a number is  : "+squareroot);
+	}
+	}
+
